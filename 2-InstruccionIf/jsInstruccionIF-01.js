4c9dbdbb951302a34951739266c84d50317a9562
BraianAquije <86858566+BraianAquije@users.noreply.github.com>
2021-07-14 22:50:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,25 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = prompt("Ingrese su edad: ");
+	edad = parseInt(edad);
+	console.log(typeof edad);
+
+	if(edad == 15)
+	{
+		mensaje = "Niña bonita"
+		document.getElementById("txtIdEdad").value = mensaje ;
+	}
 
 }//FIN DE LA FUNCIÓN
+
+
+
+// 18 numero
+// "18" string
+
+// == valida el contenido 
+// === valida tipo y contenido
+// != distinto de
+// !== distinto tipo y dato
