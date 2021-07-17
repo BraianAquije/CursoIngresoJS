6e8038e35a57aedb673876d3bd8340972cdc903e
BraianAquije <86858566+BraianAquije@users.noreply.github.com>
2021-07-17 13:12:20 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	/*Math.random()= resultado al azar entre 0 y 1
+ 	Math.floor()= redondea al entero inferior
+	Math.ceil()= redondea al entero superior
+	Math.round()= redondea al numero mas cercano*/
+
+	let num;
+	let minimo=1;
+	let maximo=10;
+
+	num = Math.round(Math.random() * (maximo -minimo) + minimo);
+
+	alert(num);
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
+	let minimo=1;
+	let maximo=10;
+
+	nota = Math.round(Math.random() * (maximo - minimo)+minimo);
+	
+	if (nota>=9)
+	{
+		alert("Su nota es "+nota+" : ¡¡EXCELENTE!!");
+	}
+	else if (nota>=4)
+	{
+		alert("Su nota es "+nota+" : ¡APROBÓ!");
+	}
+	else
+	{
+		alert("Su nota es "+nota+" : ¡VAMOS QUE LA PROXIMA SE PUEDE!");
+	}
+
 
 }//FIN DE LA FUNCIÓN
