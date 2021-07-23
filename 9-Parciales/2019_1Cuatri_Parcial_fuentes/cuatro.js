dda16066cf7b0affa5b331a84afce785e7915171
BraianAquije <86858566+BraianAquije@users.noreply.github.com>
2021-07-23 06:19:37 -0300

    Add files via upload

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,28 @@
-function mostrar()
-{
+function mostrar() {
+    let num1;
+    let num2;
+    let operacionSuma;
+    let operacionResta;
 
+    num1 = parseInt(prompt("Ingrese un numero"));
+    num2 = parseInt(prompt("Ingrese un segundo numero"))
+    operacionSuma = num1 + num2;
+    operacionResta = num1 - num2;
+
+    if (num1 == num2) {
+        alert("El primer numero " + num1 + " es igual al segundo numero " + num2);
+    }
+    else {
+        if (num1 < num2) {
+            alert("El resultado de la suma es: " + operacionSuma);
+        }
+        else {
+            if (num1 > num2 && operacionResta > 10) {
+                alert("La resta es: " + operacionResta + " y superó el 10");
+            }
+            else {
+                alert("La resta es: " + operacionResta);
+            }
+        }
+    }
 }
