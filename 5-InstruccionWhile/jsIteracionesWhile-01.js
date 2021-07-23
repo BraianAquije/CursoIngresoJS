7eed6f5a862a887ceaee9c05cf36a4fc688a71e3
BraianAquije <86858566+BraianAquije@users.noreply.github.com>
2021-07-23 06:22:22 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,10 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let i =0; // variable de control
+
+	while (i<10){ //utilizar la variable de control
+		i =i+1; // controlar la variable de control
+		alert("i: "+i);
+	}
 }//FIN DE LA FUNCIÓN
