b708451da634303102625251338e5de9257bbd79
BraianAquije <86858566+BraianAquije@users.noreply.github.com>
2021-07-23 06:19:01 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+let num1;
+let num2;
+let suma;
+let dividir;
 
+num1 = parseInt(prompt("Ingrese un numero"));
+num2 = parseInt(prompt("Ingrese un segundo numero"));
+suma = num1+num2;
+dividir = num1 / num2;
+
+if (num1 == num2)
+{
+    alert("El numero "+num1+" es igual al numero "+num2);
+}
+else
+{
+    if(num1 > num2)
+    {
+        alert("La división es " +dividir);
+    }
+    else
+    {
+       if (num1<num2&&suma<50)
+       {
+        alert("la suma es "+suma+ " y es menor a 50");
+       }
+       else
+       {
+           alert("la suma es "+suma);
+       }
+    }
+}
 }
